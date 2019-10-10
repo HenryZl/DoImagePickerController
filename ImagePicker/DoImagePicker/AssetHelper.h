71375728c20764f3c5b01bc34ec9a302247e020c
Henry Zhang <1095687324@qq.com>
2019-10-10 16:17:57 +0800

    更新支持iOS13
    1.删除-(void)getSavedPhotoList;和-(void)getCroppedImage;方法实现
    2.使用PhotoKit代替ALAssetLibrary，更新AssetHelper，使之支持iOS13以上。

--- ImagePicker/DoImagePicker/AssetHelper.h
+++ ImagePicker/DoImagePicker/AssetHelper.h
@@ -1,5 +1,5 @@
 //
-//  AssetHelper.m
+//  AssetHelper.h
 //  DoImagePickerController
 //
 //  Created by Donobono on 2014. 1. 23..
@@ -7,30 +7,32 @@
 
 #import <Foundation/Foundation.h>
 #import <AssetsLibrary/AssetsLibrary.h>
+#import <Photos/Photos.h>
 
-#define ASSETHELPER    [AssetHelper sharedAssetHelper]
+#define ASSETHELPER [AssetHelper sharedAssetHelper]
 
-#define ASSET_PHOTO_THUMBNAIL           0
-#define ASSET_PHOTO_ASPECT_THUMBNAIL    1
-#define ASSET_PHOTO_SCREEN_SIZE         2
-#define ASSET_PHOTO_FULL_RESOLUTION     3
+#define ASSET_PHOTO_THUMBNAIL 0
+#define ASSET_PHOTO_SCREEN_SIZE 1
+#define ASSET_PHOTO_FULL_RESOLUTION 2
 
 @interface AssetHelper : NSObject
 
 - (void)initAsset;
 
-@property (nonatomic, strong)   ALAssetsLibrary			*assetsLibrary;
-@property (nonatomic, strong)   NSMutableArray          *assetPhotos;
-@property (nonatomic, strong)   NSMutableArray          *assetGroups;
+//@property (nonatomic, strong)   ALAssetsLibrary            *assetsLibrary;
+@property(nonatomic, strong) PHPhotoLibrary *phphotoLibrary;
 
-@property (readwrite)           BOOL                    bReverse;
+@property(nonatomic, strong) NSMutableArray *assetPhotos;
+@property(nonatomic, strong) NSMutableArray *assetGroups;
+
+@property(readwrite) BOOL bReverse;
 
 + (AssetHelper *)sharedAssetHelper;
 
 // get album list from asset
 - (void)getGroupList:(void (^)(NSArray *))result;
 // get photos from specific album with ALAssetsGroup object
-- (void)getPhotoListOfGroup:(ALAssetsGroup *)alGroup result:(void (^)(NSArray *))result;
+- (void)getPhotoListOfGroup:(PHCollection *)alGroup result:(void (^)(NSArray *))result;
 // get photos from specific album with index of album array
 - (void)getPhotoListOfGroupByIndex:(NSInteger)nGroupIndex result:(void (^)(NSArray *))result;
 // get photos from camera roll
@@ -44,10 +46,8 @@
 
 // utils
 - (UIImage *)getCroppedImage:(NSURL *)urlImage;
-- (UIImage *)getImageFromAsset:(ALAsset *)asset type:(NSInteger)nType;
+- (UIImage *)getImageFromAsset:(PHAsset *)asset type:(NSInteger)nType;
 - (UIImage *)getImageAtIndex:(NSInteger)nIndex type:(NSInteger)nType;
-- (ALAsset *)getAssetAtIndex:(NSInteger)nIndex;
-- (ALAssetsGroup *)getGroupAtIndex:(NSInteger)nIndex;
+- (PHAsset *)getAssetAtIndex:(NSInteger)nIndex;
 
 @end
-
--- ImagePicker/DoImagePicker/AssetHelper.m
+++ ImagePicker/DoImagePicker/AssetHelper.m
@@ -24,85 +24,69 @@
 
 - (void)initAsset
 {
-    if (self.assetsLibrary == nil)
-    {
-        _assetsLibrary = [[ALAssetsLibrary alloc] init];
-        
-        NSString *strVersion = [[UIDevice alloc] systemVersion];
-        if ([strVersion compare:@"5"] >= 0)
-            [_assetsLibrary writeImageToSavedPhotosAlbum:nil metadata:nil completionBlock:^(NSURL *assetURL, NSError *error) {
-            }];
+    
+}
+
+- (BOOL)isCameraRollAlbum:(PHAssetCollection *)metadata {
+    NSString *versionStr = [[UIDevice currentDevice].systemVersion stringByReplacingOccurrencesOfString:@"." withString:@""];
+    if (versionStr.length <= 1) {
+        versionStr = [versionStr stringByAppendingString:@"00"];
+    } else if (versionStr.length <= 2) {
+        versionStr = [versionStr stringByAppendingString:@"0"];
+    }
+    CGFloat version = versionStr.floatValue;
+    // 目前已知8.0.0 ~ 8.0.2系统，拍照后的图片会保存在最近添加中
+    if (version >= 800 && version <= 802) {
+        return ((PHAssetCollection *)metadata).assetCollectionSubtype == PHAssetCollectionSubtypeSmartAlbumRecentlyAdded;
+    } else {
+        return ((PHAssetCollection *)metadata).assetCollectionSubtype == PHAssetCollectionSubtypeSmartAlbumUserLibrary;
     }
 }
 
-- (void)setCameraRollAtFirst
-{
-    for (ALAssetsGroup *group in _assetGroups)
-    {
-        if ([[group valueForProperty:@"ALAssetsGroupPropertyType"] intValue] == ALAssetsGroupSavedPhotos)
-        {
-            // send to head
-            [_assetGroups removeObject:group];
-            [_assetGroups insertObject:group atIndex:0];
-            
-            return;
-        }
-    }
-}
 
 - (void)getGroupList:(void (^)(NSArray *))result
 {
     [self initAsset];
     
-    void (^assetGroupEnumerator)(ALAssetsGroup *, BOOL *) = ^(ALAssetsGroup *group, BOOL *stop)
-    {
-        [group setAssetsFilter:[ALAssetsFilter allPhotos]];
-
-        if (group == nil)
-        {
-            if (_bReverse)
-                _assetGroups = [[NSMutableArray alloc] initWithArray:[[_assetGroups reverseObjectEnumerator] allObjects]];
-            
-            [self setCameraRollAtFirst];
-            
-            // end of enumeration
-            result(_assetGroups);
-            return;
-        }
-        
-        [_assetGroups addObject:group];
-    };
-    
-    void (^assetGroupEnumberatorFailure)(NSError *) = ^(NSError *error)
-    {
-        NSLog(@"Error : %@", [error description]);
-    };
-    
     _assetGroups = [[NSMutableArray alloc] init];
-    [_assetsLibrary enumerateGroupsWithTypes:ALAssetsGroupAll
-                                  usingBlock:assetGroupEnumerator
-                                failureBlock:assetGroupEnumberatorFailure];
+    PHFetchOptions *option = [[PHFetchOptions alloc] init];
+    option.predicate = [NSPredicate predicateWithFormat:@"mediaType == %ld", PHAssetMediaTypeImage];
+   
+    PHFetchResult *smartAlbums = [PHAssetCollection fetchAssetCollectionsWithType:PHAssetCollectionTypeSmartAlbum subtype:PHAssetCollectionSubtypeAlbumRegular options:nil];
+    
+    for (PHAssetCollection *collection in smartAlbums) {
+        // 有可能是PHCollectionList类的的对象，过滤掉
+        if (![collection isKindOfClass:[PHAssetCollection class]]) continue;
+        // 过滤空相册
+        if (collection.estimatedAssetCount <= 0) continue;
+        if (collection.assetCollectionSubtype == PHAssetCollectionSubtypeSmartAlbumAllHidden) continue;
+        if (collection.assetCollectionSubtype == 1000000201) continue; //『最近删除』相册
+        PHFetchResult *fetchResult = [PHAsset fetchAssetsInAssetCollection:collection options:option];
+        if (fetchResult.count < 1 && ![self isCameraRollAlbum:collection]) continue;
+        
+        [_assetGroups addObject:collection];
+        
+    }
+    result(_assetGroups);
+
 }
 
-- (void)getPhotoListOfGroup:(ALAssetsGroup *)alGroup result:(void (^)(NSArray *))result
+- (void)getPhotoListOfGroup:(PHCollection *)collection result:(void (^)(NSArray *))result
 {
     [self initAsset];
     
     _assetPhotos = [[NSMutableArray alloc] init];
-    [alGroup setAssetsFilter:[ALAssetsFilter allPhotos]];
-    [alGroup enumerateAssetsUsingBlock:^(ALAsset *alPhoto, NSUInteger index, BOOL *stop) {
-        
-        if(alPhoto == nil)
-        {
-            if (_bReverse)
-                _assetPhotos = [[NSMutableArray alloc] initWithArray:[[_assetPhotos reverseObjectEnumerator] allObjects]];
-            
-            result(_assetPhotos);
-            return;
+    PHFetchOptions *option = [[PHFetchOptions alloc] init];
+    option.predicate = [NSPredicate predicateWithFormat:@"mediaType == %ld", PHAssetMediaTypeImage];
+    PHFetchResult *fetchResult = [PHAsset fetchAssetsInAssetCollection:(PHAssetCollection *)collection options:option];
+    if (fetchResult.count > 0) {
+        //        [_assetPhotos addObjectsFromArray:fetchResult];
+        for (NSInteger i = 0; i < [fetchResult count]; i++) {
+            [_assetPhotos addObject:fetchResult[i]];
         }
+        result(_assetPhotos);
         
-        [_assetPhotos addObject:alPhoto];
-    }];
+    }
 }
 
 - (void)getPhotoListOfGroupByIndex:(NSInteger)nGroupIndex result:(void (^)(NSArray *))result
@@ -116,43 +100,43 @@
 
 - (void)getSavedPhotoList:(void (^)(NSArray *))result error:(void (^)(NSError *))error
 {
-    [self initAsset];
-    
-    dispatch_async(dispatch_get_main_queue(), ^{
-
-        void (^assetGroupEnumerator)(ALAssetsGroup *, BOOL *) = ^(ALAssetsGroup *group, BOOL *stop)
-        {
-            if ([[group valueForProperty:@"ALAssetsGroupPropertyType"] intValue] == ALAssetsGroupSavedPhotos)
-            {
-                [group setAssetsFilter:[ALAssetsFilter allPhotos]];
-
-                [group enumerateAssetsUsingBlock:^(ALAsset *alPhoto, NSUInteger index, BOOL *stop) {
-                    
-                    if(alPhoto == nil)
-                    {
-                        if (_bReverse)
-                            _assetPhotos = [[NSMutableArray alloc] initWithArray:[[_assetPhotos reverseObjectEnumerator] allObjects]];
-                        
-                        result(_assetPhotos);
-                        return;
-                    }
-                    
-                    [_assetPhotos addObject:alPhoto];
-                }];
-            }
-        };
-        
-        void (^assetGroupEnumberatorFailure)(NSError *) = ^(NSError *err)
-        {
-            NSLog(@"Error : %@", [err description]);
-            error(err);
-        };
-        
-        _assetPhotos = [[NSMutableArray alloc] init];
-        [_assetsLibrary enumerateGroupsWithTypes:ALAssetsGroupSavedPhotos
-                                      usingBlock:assetGroupEnumerator
-                                    failureBlock:assetGroupEnumberatorFailure];
-    });
+//    [self initAsset];
+//
+//    dispatch_async(dispatch_get_main_queue(), ^{
+//
+//        void (^assetGroupEnumerator)(ALAssetsGroup *, BOOL *) = ^(ALAssetsGroup *group, BOOL *stop)
+//        {
+//            if ([[group valueForProperty:@"ALAssetsGroupPropertyType"] intValue] == ALAssetsGroupSavedPhotos)
+//            {
+//                [group setAssetsFilter:[ALAssetsFilter allPhotos]];
+//
+//                [group enumerateAssetsUsingBlock:^(ALAsset *alPhoto, NSUInteger index, BOOL *stop) {
+//
+//                    if(alPhoto == nil)
+//                    {
+//                        if (_bReverse)
+//                            _assetPhotos = [[NSMutableArray alloc] initWithArray:[[_assetPhotos reverseObjectEnumerator] allObjects]];
+//
+//                        result(_assetPhotos);
+//                        return;
+//                    }
+//
+//                    [_assetPhotos addObject:alPhoto];
+//                }];
+//            }
+//        };
+//
+//        void (^assetGroupEnumberatorFailure)(NSError *) = ^(NSError *err)
+//        {
+//            NSLog(@"Error : %@", [err description]);
+//            error(err);
+//        };
+//
+//        _assetPhotos = [[NSMutableArray alloc] init];
+//        [_assetsLibrary enumerateGroupsWithTypes:ALAssetsGroupSavedPhotos
+//                                      usingBlock:assetGroupEnumerator
+//                                    failureBlock:assetGroupEnumberatorFailure];
+//    });
 }
 
 - (NSInteger)getGroupCount
@@ -167,8 +151,36 @@
 
 - (NSDictionary *)getGroupInfo:(NSInteger)nIndex
 {
-    return @{@"name" : [_assetGroups[nIndex] valueForProperty:ALAssetsGroupPropertyName],
-             @"count" : @([_assetGroups[nIndex] numberOfAssets])};
+    
+    PHAssetCollection *collection = [_assetGroups objectAtIndex:nIndex];
+    PHFetchOptions *option = [[PHFetchOptions alloc] init];
+    option.predicate = [NSPredicate predicateWithFormat:@"mediaType == %ld", PHAssetMediaTypeImage];
+    PHFetchResult *fetchResult = [PHAsset fetchAssetsInAssetCollection:(PHAssetCollection *)collection options:option];
+    
+    PHFetchResult *assetsFetchResult = [PHAsset fetchAssetsInAssetCollection:collection options:nil];
+    PHAsset *asset = [assetsFetchResult firstObject];
+    PHImageRequestOptions *options = [[PHImageRequestOptions alloc] init];
+
+    options.synchronous = YES;
+    options.resizeMode = PHImageRequestOptionsResizeModeExact;
+    CGFloat scale = [UIScreen mainScreen].scale;
+    CGFloat dimension = 128.0f;
+    CGSize size = CGSizeMake(dimension*scale, dimension*scale);
+    __block UIImage *posterImage = [UIImage imageNamed:@"loading_square_120px"];
+    [[PHImageManager defaultManager] requestImageForAsset:asset targetSize:size contentMode:PHImageContentModeDefault options:options resultHandler:^(UIImage *result, NSDictionary *info) {
+        if ([[info valueForKey:@"PHImageResultIsDegradedKey"]integerValue]==0){
+            if (result) {
+                posterImage = result;
+            }
+        } else {
+            // Do something with the regraded image
+        }
+        
+    }];
+    return @{@"name" : collection.localizedTitle,
+             @"count" : @([fetchResult count]),
+             @"thumbnail" : posterImage};
+   
 }
 
 - (void)clearData
@@ -181,119 +193,101 @@
 - (UIImage *)getCroppedImage:(NSURL *)urlImage
 {
     __block UIImage *iImage = nil;
-    __block BOOL bBusy = YES;
-    
-    ALAssetsLibraryAssetForURLResultBlock resultblock = ^(ALAsset *myasset)
-    {
-        ALAssetRepresentation *rep = [myasset defaultRepresentation];
-        NSString *strXMP = rep.metadata[@"AdjustmentXMP"];
-        if (strXMP == nil || [strXMP isKindOfClass:[NSNull class]])
-        {
-            CGImageRef iref = [rep fullResolutionImage];
-            if (iref)
-                iImage = [UIImage imageWithCGImage:iref scale:1.0 orientation:(UIImageOrientation)rep.orientation];
-            else
-                iImage = nil;
-        }
-        else
-        {
-            // to get edited photo by photo app
-            NSData *dXMP = [strXMP dataUsingEncoding:NSUTF8StringEncoding];
-            
-            CIImage *image = [CIImage imageWithCGImage:rep.fullResolutionImage];
-            
-            NSError *error = nil;
-            NSArray *filterArray = [CIFilter filterArrayFromSerializedXMP:dXMP
-                                                         inputImageExtent:image.extent
-                                                                    error:&error];
-            if (error) {
-                NSLog(@"Error during CIFilter creation: %@", [error localizedDescription]);
-            }
-            
-            for (CIFilter *filter in filterArray) {
-                [filter setValue:image forKey:kCIInputImageKey];
-                image = [filter outputImage];
-            }
-            
-            iImage = [UIImage imageWithCIImage:image scale:1.0 orientation:(UIImageOrientation)rep.orientation];
-        }
-        
-		bBusy = NO;
-    };
-    
-    ALAssetsLibraryAccessFailureBlock failureblock  = ^(NSError *myerror)
-    {
-        NSLog(@"booya, cant get image - %@",[myerror localizedDescription]);
-    };
-    
-    [_assetsLibrary assetForURL:urlImage
-                    resultBlock:resultblock
-                   failureBlock:failureblock];
-    
-	while (bBusy)
-		[[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate distantFuture]];
-    
+//    __block BOOL bBusy = YES;
+//
+//    ALAssetsLibraryAssetForURLResultBlock resultblock = ^(ALAsset *myasset)
+//    {
+//        ALAssetRepresentation *rep = [myasset defaultRepresentation];
+//        NSString *strXMP = rep.metadata[@"AdjustmentXMP"];
+//        if (strXMP == nil || [strXMP isKindOfClass:[NSNull class]])
+//        {
+//            CGImageRef iref = [rep fullResolutionImage];
+//            if (iref)
+//                iImage = [UIImage imageWithCGImage:iref scale:1.0 orientation:(UIImageOrientation)rep.orientation];
+//            else
+//                iImage = nil;
+//        }
+//        else
+//        {
+//            // to get edited photo by photo app
+//            NSData *dXMP = [strXMP dataUsingEncoding:NSUTF8StringEncoding];
+//
+//            CIImage *image = [CIImage imageWithCGImage:rep.fullResolutionImage];
+//
+//            NSError *error = nil;
+//            NSArray *filterArray = [CIFilter filterArrayFromSerializedXMP:dXMP
+//                                                         inputImageExtent:image.extent
+//                                                                    error:&error];
+//            if (error) {
+//                NSLog(@"Error during CIFilter creation: %@", [error localizedDescription]);
+//            }
+//
+//            for (CIFilter *filter in filterArray) {
+//                [filter setValue:image forKey:kCIInputImageKey];
+//                image = [filter outputImage];
+//            }
+//
+//            iImage = [UIImage imageWithCIImage:image scale:1.0 orientation:(UIImageOrientation)rep.orientation];
+//        }
+//
+//        bBusy = NO;
+//    };
+//
+//    ALAssetsLibraryAccessFailureBlock failureblock  = ^(NSError *myerror)
+//    {
+//        NSLog(@"booya, cant get image - %@",[myerror localizedDescription]);
+//    };
+//
+//    [_assetsLibrary assetForURL:urlImage
+//                    resultBlock:resultblock
+//                   failureBlock:failureblock];
+//
+//    while (bBusy)
+//        [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate distantFuture]];
+//
     return iImage;
 }
 
-- (UIImage *)getImageFromAsset:(ALAsset *)asset type:(NSInteger)nType
+- (UIImage *)getImageFromAsset:(PHAsset *)asset type:(NSInteger)nType
 {
-    CGImageRef iRef = nil;
+    PHImageRequestOptions *options = [[PHImageRequestOptions alloc] init];
+    //默认的是异步加载,这里选择了同步
+    options.synchronous = YES;
+    //PHImageManagerMaximumSize:获取原图,占用很大内存 建议不要使用
+    CGSize tagetSize = PHImageManagerMaximumSize;
     
     if (nType == ASSET_PHOTO_THUMBNAIL)
-        iRef = [asset thumbnail];
-    else if (nType == ASSET_PHOTO_ASPECT_THUMBNAIL)
-        iRef = [asset aspectRatioThumbnail];
+        tagetSize = CGSizeMake(128.f, 128.f);
     else if (nType == ASSET_PHOTO_SCREEN_SIZE)
-        iRef = [asset.defaultRepresentation fullScreenImage];
+        tagetSize = PHImageManagerMaximumSize;
     else if (nType == ASSET_PHOTO_FULL_RESOLUTION)
     {
-        NSString *strXMP = asset.defaultRepresentation.metadata[@"AdjustmentXMP"];
-        if (strXMP == nil || [strXMP isKindOfClass:[NSNull class]])
-        {
-            iRef = [asset.defaultRepresentation fullResolutionImage];
-            return [UIImage imageWithCGImage:iRef scale:1.0 orientation:(UIImageOrientation)asset.defaultRepresentation.orientation];
-        }
-        else
-        {
-            NSData *dXMP = [strXMP dataUsingEncoding:NSUTF8StringEncoding];
-            
-            CIImage *image = [CIImage imageWithCGImage:asset.defaultRepresentation.fullResolutionImage];
-            
-            NSError *error = nil;
-            NSArray *filterArray = [CIFilter filterArrayFromSerializedXMP:dXMP
-                                                         inputImageExtent:image.extent
-                                                                    error:&error];
-            if (error) {
-                NSLog(@"Error during CIFilter creation: %@", [error localizedDescription]);
-            }
-            
-            for (CIFilter *filter in filterArray) {
-                [filter setValue:image forKey:kCIInputImageKey];
-                image = [filter outputImage];
-            }
-            
-            UIImage *iImage = [UIImage imageWithCIImage:image scale:1.0 orientation:(UIImageOrientation)asset.defaultRepresentation.orientation];
-            return iImage;
-        }
+        tagetSize = PHImageManagerMaximumSize;
     }
     
-    return [UIImage imageWithCGImage:iRef];
+    __block UIImage *targetImage = [UIImage new];
+    [[PHImageManager defaultManager] requestImageForAsset:asset targetSize:tagetSize contentMode:PHImageContentModeAspectFit options:options resultHandler:^(UIImage * _Nullable result, NSDictionary * _Nullable info)
+     {
+         if ([[info valueForKey:@"PHImageResultIsDegradedKey"]integerValue]==0){
+             if (result) {
+                 targetImage = result;
+             }
+         } else {
+             // Do something with the regraded image
+         }
+     }];
+    
+    return targetImage;
 }
 
 - (UIImage *)getImageAtIndex:(NSInteger)nIndex type:(NSInteger)nType
 {
-    return [self getImageFromAsset:(ALAsset *)_assetPhotos[nIndex] type:nType];
+    return [self getImageFromAsset:(PHAsset *) _assetPhotos[nIndex] type:nType];
 }
 
-- (ALAsset *)getAssetAtIndex:(NSInteger)nIndex
+- (PHAsset *)getAssetAtIndex:(NSInteger)nIndex
 {
     return _assetPhotos[nIndex];
-}
-
-- (ALAssetsGroup *)getGroupAtIndex:(NSInteger)nIndex
-{
-    return _assetGroups[nIndex];
 }
 
 @end
